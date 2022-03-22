4a9eec9393fa50ae38b55c4dd78a99b70433905c
Kiran Thomas <thomas.kiran@heb.com>
2022-03-21 19:07:03 -0500

    chore: fix typo for default import of redisClient

--- src/middlewares/get-access-token.ts
+++ src/middlewares/get-access-token.ts
@@ -1,10 +1,10 @@
 import type Koa from "koa";
 import client from "../clients/oauth";
-import redistClient from "../clients/redis";
+import redisClient from "../clients/redis";
 
 export default function getAccessToken(): Koa.Middleware {
   return async (ctx, next): Promise<void> => {
-    const accessTokenJSONString = (await redistClient.get("accessToken")) || "";
+    const accessTokenJSONString = (await redisClient.get("accessToken")) || "";
 
     let accessToken = client.createToken(JSON.parse(accessTokenJSONString));
     if (accessToken.expired()) {
@@ -16,7 +16,7 @@ export default function getAccessToken(): Koa.Middleware {
         console.log("Error refreshing access token: ", error);
       }
 
-      redistClient.set("accessToken", JSON.stringify(accessToken));
+      redisClient.set("accessToken", JSON.stringify(accessToken));
     }
 
     ctx.state.accessToken = accessToken;
