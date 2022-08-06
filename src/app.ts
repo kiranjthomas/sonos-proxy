ed4abdc08ceb0121b4dee63be7a7eb710159da81
Kiran Thomas <kirthoma@cisco.com>
2022-08-06 10:49:47 -0500

    feat: serve static files using koa-mount and koa-static

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import Koa from "koa";
 import cors from "@koa/cors";
+import serve from "koa-static";
+import mount from "koa-mount";
+
 import { oauthRouter, controlRouter, healthRouter } from "./routes";
+import { healthRouter } from "./routes";
 
 const app = new Koa();
 
 app.use(cors());
+
+console.log(`${__dirname}`)
+app.use(mount('/', serve(__dirname + '/sonos-client')))
 
 app.use(healthRouter.routes());
 app.use(controlRouter.routes());
