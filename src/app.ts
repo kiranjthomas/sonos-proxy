2ee94c22d29327819dd32115565a6a231afb4bce
Kiran Thomas <kirthoma@cisco.com>
2024-07-23 12:14:12 -0500

    feat: add middleware logger for requests

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import cors from "@koa/cors";
 import serve from "koa-static";
 import mount from "koa-mount";
 import bodyParser from "@koa/bodyparser";
+import logger from "koa-logger";
 
 import { oauthRouter, controlRouter, healthRouter } from "./routes";
 
 const app = new Koa();
 
+app.use(logger());
 app.use(cors());
 app.use(bodyParser());
 
